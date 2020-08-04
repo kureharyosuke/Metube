19d9ed9706aa1cd6643e2220bad2aaea65dcc15b
kureharyosuke <ohhayo.com@gmail.com>
2020-08-04 16:56:16 +0900

    0-4 cookie & body Parser Install

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -9,6 +13,12 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(logger("dev"));
 
 app.get("/", handleHome);
 
